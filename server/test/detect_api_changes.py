232aec6aaff5ed7fd2a7277c7353dc95b3a6f2b8
Akiff Manji <akiff.manji@gmail.com>
2021-02-27 15:35:04 -0800

    Script will make a request and strip dates from the result set
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- server/test/detect_api_changes.py
+++ server/test/detect_api_changes.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import csv, json
+import csv
+import json
 import pip._vendor.requests as requests
 
 queries = {}
@@ -32,9 +33,37 @@ def populate_queries(csvfile):
         tag, desc = parse_comment(rowstr)
         if (tag != '' and desc != ''):
             last = tag
-            queries[tag] = {'desc': desc, 'entries': []}
+            queries[tag] = {'desc': desc, 'queries': [], 'entries': []}
             continue
-        queries[last]['entries'].append(rowstr)
+        queries[last]['queries'].append(rowstr)
+
+
+def remove_dates(obj):
+    """
+    Recursively removes dates from the result
+    """
+    if (not obj):
+        return
+    if isinstance(obj, list):
+        for subobj in obj:
+            remove_dates(subobj)
+    else:
+        for k, v in obj.copy().items():
+            if isinstance(v, object) and not isinstance(v, (int, str, bool)):
+                remove_dates(v)
+            else:
+                if (has_timestamp_key(k)):
+                    obj.pop(k)
+                if (has_timestamp_value(k, v)):
+                    obj.pop('value')
+
+
+def has_timestamp_key(k):
+    return k in ('create_timestamp', 'update_timestamp', 'last_updated')
+
+
+def has_timestamp_value(k, v):
+    return ((k == 'type') and v in ('registration_date', 'entity_name_effective', 'entity_name_assumed_effective', 'entity_status_effective', 'relationship_status_effective'))
 
 
 if __name__ == "__main__":
@@ -42,10 +71,20 @@ if __name__ == "__main__":
         reader = csv.reader(csvfile)
         populate_queries(csvfile)
 
-    for query in queries['entity_type']['entries']:
+    for query in queries['entity_type']['queries']:
         params = query.split(',')
         source_id = params[0].strip()
         type = params[1].strip()
+        entry = {
+            'url': f'http://localhost:8081/api/topic/ident/{type}/{source_id}/formatted',
+        }
+        r = requests.get(entry['url'])
+        res = r.json()
+        remove_dates(res)
 
-        r = requests.get(f'http://localhost:8080/api/topic/ident/{type}/{source_id}/formatted')
-        print(r.json())
+        entry['result'] = res
+        entry['result_str'] = json.dumps(res)
+        queries['entity_type']['entries'].append(entry)
+
+    with open('local-corp-test-sample-corps.json', 'w') as jsonfile:
+        jsonfile.write(json.dumps(queries))
